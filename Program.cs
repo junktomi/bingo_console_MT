9b70db02e1e263bde55dffdea333ba71c439fd9d
junktomi <junktomi0227@protonmail.com>
2026-03-04 08:29:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bingo_console_MT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
