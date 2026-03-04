dfb8be8789daa3d8ef05fb64b6cddffda7ab252c
junktomi <junktomi0227@protonmail.com>
2026-03-04 08:39:17 +0100

    1. feladat: BingoJatekos osztály

---
+++ BingoJatekos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bingo_console_MT
+{
+    internal class BingoJatekos
+    {
+        public string Nev { get; set; }
+        public string[,] Kartya { get; set; }
+        public bool[,] Jeloles { get; set; }
+
+        public BingoJatekos(string nev, string[,] kartya)
+        {
+            Nev = nev;
+            Kartya = kartya;
+            Jeloles = new bool[5, 5];
+
+        }
+    }
+}
